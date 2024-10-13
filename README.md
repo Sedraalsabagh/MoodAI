c5b3c7398d544dc701d7c1a4b8006731454a0907
Sedraalsabagh <sedraalsabbagh2021@gmail.com>
2024-10-13 23:45:04 +0300

    new

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MoodAI
+![مود](https://github.com/user-attachments/assets/d377a8a2-4938-49fe-918e-494dc0280bd5)
+![ساااا](https://github.com/user-attachments/assets/5a8b09f5-668e-4e2e-bb78-a8cabcf31344)
---
+++ app.py
@@ -0,0 +1,53 @@
+from tensorflow.keras.models import load_model
+from fastapi import FastAPI, File, UploadFile
+from PIL import Image
+import numpy as np
+import io
+
+
+model = load_model("history.h5")
+
+
+app = FastAPI()
+
+
+def preprocess_image(image: Image.Image) -> np.ndarray:
+    
+    if image.mode != 'L':
+        image = image.convert('L')
+    
+    
+    image = image.resize((48, 48))
+    
+    
+    img_array = np.array(image)
+    img_array = img_array.astype('float32') / 255.0  
+    img_array = np.expand_dims(img_array, axis=-1) 
+    img_array = np.expand_dims(img_array, axis=0)   
+    
+    return img_array
+
+@app.post("/predict-emotion/")
+async def predict_emotion(file: UploadFile = File(...)):
+    
+    contents = await file.read()
+    image = Image.open(io.BytesIO(contents))
+    
+   
+    processed_image = preprocess_image(image)
+    
+   
+    prediction = model.predict(processed_image)
+    
+   
+    predicted_emotion = np.argmax(prediction, axis=1)[0]
+    
+  
+    emotion_labels = ['Anger', 'Disgust', 'Fear', 'Happiness', 'Sadness', 'Surprise', 'Neutral']
+    
+    return {"emotion": emotion_labels[predicted_emotion]}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
